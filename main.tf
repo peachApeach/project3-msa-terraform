b7b7f9ef4a94b4eeed817843649d4cec847d6ce1
peachApeach <yh_01@daum.net>
2023-05-30 21:17:19 +0900

    ADD: API Gateway - Lambda

--- main.tf
+++ main.tf
@@ -67,8 +67,8 @@ resource "aws_iam_role" "salesapilambda_exec" {
 
 resource "aws_iam_policy" "salesapi-role" {
   name = "SalesAPI-ap-notrheast-2-lambdaRole"
-
-  policy = <<POLICY
+  // 공백 무시 
+  policy = <<-POLICY
   {
     "Version": "2012-10-17",
     "Statement": [{
@@ -91,5 +91,58 @@ resource "aws_iam_role_policy_attachment" "salesapi-policy" {
   policy_arn = aws_iam_policy.salesapi-role.arn
 }
 
+##############################################################
+# Sales API Gateway Trigger
+##############################################################
+# version 1은 REST API를 생성할 때 사용하며 version 2는 WebSocket 및 HTTP API를 생성하고 배포하는데 사용 
+resource "aws_apigatewayv2_api" "salesapi-tf-gw" {
+  name = "salesapi-tf-gw"
+  protocol_type = "HTTP"
 
+  # HTTP 구성에만 사용 가능 
+  cors_configuration {
+    # 아래 옵션들은 모두 선택사항이므로 필요한 옵션을 지정 
+    allow_credentials = false                               # CORS 자격 증명 포함 여부
+    allow_headers = [  ]                                    # 허용되는 HTTP header 집합 
+    allow_methods = [ "*" ]                                 # 허용되는 메서드 집합 
+    allow_origins = [ "*" ]                                 # 허용되는 origin 집합
+    expose_headers = [ ]                                    # 노출되는 header 집합
+    max_age = 0                                             # 브라우저가 실행 전 요청 결과를 캐시해야하는 시간(초)
+  }
+}
 
+# API 게이트웨이에 대한 어플리케이션 단계를 설정
+resource "aws_apigatewayv2_stage" "salesapi_stage" {
+  api_id = aws_apigatewayv2_api.salesapi-tf-gw.id
+
+  name = "$default"
+  auto_deploy = "false"
+
+}
+
+# Lambda 함수를 사용하도록 API 게이트웨이를 구성 
+resource "aws_apigatewayv2_integration" "salesapi_integration" {
+  api_id = aws_apigatewayv2_api.salesapi-tf-gw.id
+
+  integration_uri = aws_lambda_function.sales-api-lambda.invoke_arn
+  integration_type = "AWS_PROXY"
+}
+
+# HTTP 요청을 Lambda 함수에 매핑
+resource "aws_apigatewayv2_route" "salesapi_route" {
+    api_id = aws_apigatewayv2_api.salesapi-tf-gw.id
+    route_key = "$default"
+    # 경로에 대한 대상 integrations/{IntegrationID}
+    target = "integrations/${aws_apigatewayv2_integration.salesapi_integration.id}"
+  
+}
+
+# Lambda 함수를 호출할 수 있는 API Gateway 권한 부여 
+resource "aws_lambda_permission" "salesapi_gwpm" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sales-api-lambda.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.salesapi-tf-gw.execution_arn}/*/*"
+}
